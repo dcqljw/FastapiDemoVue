e211554ebce9a70cbd20693f8c8ee79dd4a796c4
dcqljw <dcq0516@gmail.com>
2025-09-02 10:41:04 +0800

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 
+import AppConfigurator from "@/components/AppConfigurator.vue";
 </script>
 
 <template>
-  <div class="flex justify-between px-8 py-2 bg-[#ffffff]">
-    <div>
-      FastApi Demo
-    </div>
-    <div>
-      <Avatar label="D"/>
-    </div>
+  <div class="header">
+    <Button
+        v-styleclass="{ selector: '@next', enterFromClass: 'hidden', enterActiveClass: 'animate-scalein', leaveToClass: 'hidden', leaveActiveClass: 'animate-fadeout', hideOnOutsideClick: true }"
+        type="button"
+        class="layout-topbar-action layout-topbar-action-highlight"
+    >
+      <i class="pi pi-palette"></i>
+    </Button>
+    <AppConfigurator/>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div>
-    Hello world
+
   </div>
 </template>
+
+<script setup lang="ts">
+</script>
+<style scoped>
+</style>
